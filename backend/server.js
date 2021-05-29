da956a350bec167c7034443579db043d184feb76
Murtaza Vasi <Murtaza-Vasi>
2021-05-29 17:55:47 +0530

    preparing for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cors from 'cors';
 
@@ -16,6 +17,19 @@ app.use(express.json());
 app.post('/users', register);
 app.post('/users/login', login);
 
-app.listen(process.env.PORT, () => {
+// If any other request comes other than the routes defined then send the homepage
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+	});
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
 	console.log(`Server is running on port ${process.env.PORT}`);
 });
